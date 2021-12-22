6ba41926964b2933c74dc93ae6ebbd1035f934f5
Adam Nash <adam@smr.llc>
2021-12-22 16:41:17 -0500

    fix GUI window resize behavior #2

--- sketch.js
+++ sketch.js
@@ -3,6 +3,8 @@ let buf = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 let connections = [];
 
+let isRecording = false;
+
 function setup() {
 	//Create a canvas of dimensions given by current browser window
 	createCanvas(windowWidth, windowHeight);
@@ -10,9 +12,7 @@ function setup() {
 	//text font
 	textFont('Courier New');
 
-	// qualitySlider = createSlider(1, 32, 8);
-
-	monitorSelfToggle = createCheckbox("Monitor Self", false);
+	monitorSelfToggle = createCheckbox("Monitor", false);
 	monitorSelfSlider = createSlider(1, 1000, 900);
   	monitorSelfToggle.changed(checkEnabled);
 
@@ -24,8 +24,33 @@ function setup() {
 	leaveSessionBtn = createButton('Leave Session');
 	leaveSessionBtn.mousePressed(leaveSession);
 
+	recordBtn = createButton('Record');
+	recordBtn.mousePressed(record);
+
 	formatDOMElements();
 	checkEnabled();
+}
+
+function drawRecordButton() {
+
+	let rowH = 0.025 * windowHeight;
+	let colW = 0.05 * windowWidth
+
+	var txt
+	if(isRecording){
+		fill("red");
+		txt = "Recording..."
+	}
+	else{
+		fill("grey");
+		txt = "Record"
+	}
+	noStroke();
+	radius = Math.min(rowH*.75, colW)
+	ellipse(colW*15, rowH*5.5, radius*2, radius*2);
+
+	fill(50);
+	text(txt, 10, 10, 70, 80);
 }
 
 function drawLevelBar(x, y, maxW, maxH, avg, peak, heldPeak) {
@@ -43,22 +68,36 @@ function drawLevelBar(x, y, maxW, maxH, avg, peak, heldPeak) {
 	rect(x + maxW * .99 * heldPeak, y, maxW*0.01, maxH);
 }
 
-function createConnection(x, y, w, h, readBuf, writeBuf) {
-	let rowH = 0.25 * h;
-	let colW = 0.05 * w
+function createConnection() {
 
 	let rxLevelSlider = createSlider(1, 1000, 900);
 	let rxQueueSlider = createSlider(1, 32, 8);
-	elRect(rxLevelSlider, colW*12, y + rowH, colW*7, rowH);
-	elRect(rxQueueSlider, colW*4, y + rowH*2, colW*15, rowH);
 
 	return {
-		x: x,
-		y: y,
-		w: w,
-		h: h,
+		x: 0,
+		y: 0,
+		w: 200,
+		h: 100,
 		levelSlider: rxLevelSlider,
 		queueSlider: rxQueueSlider
+	}
+}
+
+function adjustConnections() {
+	let rowH = 0.025 * windowHeight;
+	let colW = 0.05 * windowWidth
+
+	for(let i = 0; i < connections.length; i++) {
+		conn = connections[i]
+		conn.x = 0
+		conn.y = rowH*9 + (i * rowH * 4.5)
+		conn.w = colW*20
+		conn.h = rowH*4
+
+		let rowC = 0.25 * conn.h;
+		let colC = 0.05 * conn.w
+		elRect(conn.levelSlider, colC*12, conn.y + rowC, colC*7, rowC);
+		elRect(conn.queueSlider, colC*4, conn.y + rowC*2, colC*15, rowC);
 	}
 }
 
@@ -95,6 +134,7 @@ function drawConnection(c, readBuf, writeBuf) {
 }
 
 function draw() {
+
     background(60);
 
 	let inL = Bela.data.buffers[1];
@@ -104,14 +144,14 @@ function draw() {
 	let colW = 0.05 * windowWidth
 
 	if (inL) {
-		drawLevelBar(colW*4, rowH*1, colW*15.0, rowH*0.5, inL[0], inL[1], inL[2]);
-		drawLevelBar(colW*4, rowH*1.5, colW*15.0, rowH*0.5, inR[0], inR[1], inR[2]);
+		drawLevelBar(colW*4.5, rowH*1, colW*14, rowH*0.5, inL[0], inL[1], inL[2]);
+		drawLevelBar(colW*4.5, rowH*1.5, colW*14, rowH*0.5, inR[0], inR[1], inR[2]);
 	}
 
 	fill(200);
 	textSize(fontSize(colW, rowH));
 	textAlign(RIGHT);
-	text("Send Level:", 0, rowH*1, colW*3.5, rowH);
+	text("Send Level:", 0, rowH*1, colW*4, rowH);
 
 	buf[0] = monitorSelfToggle.checked() ? 1 : 0;
 	buf[1] = monitorSelfSlider.value();
@@ -144,23 +184,34 @@ function draw() {
 		c.queueSlider.remove();
 	}
 	if (rxCount > 0) {
-		let readBufOffset = 10;
-		let writeBufOffset = 1;
-		let rowOffset = rowH*9;
+		let adjust = false
 		for (let i = 0; i < rxCount; i++) {
 			if ((i + 1) > connections.length) {
-				connections.push(createConnection(0, rowOffset, colW*20, rowH*4));
+				connections.push(createConnection());
+				adjust = true
 			}
+		}
+		if (adjust) {
+			adjustConnections()
+		}
+		let readBufOffset = 10;
+		let writeBufOffset = 1;
+		for (let i = 0; i < connections.length; i++) {
 			let result = drawConnection(connections[i], readBufOffset, writeBufOffset);
 			if (result < 1) {
 				continue;
 			}
-			rowOffset += rowH * 4.5;
 			readBufOffset += result;
 			writeBufOffset += 1;
 		}
 	}
 
+}
+
+function windowResized() {
+	formatDOMElements()
+	adjustConnections()
+	resizeCanvas(windowWidth, windowHeight)
 }
 
 function formatDOMElements() {
@@ -171,17 +222,20 @@ function formatDOMElements() {
 	monitorSelfToggle.style('color', color(200));
 	monitorSelfToggle.style('text-align', 'right');
 
-	elRect(monitorSelfToggle, colW, rowH*3, colW*2.5, rowH);
-	elRect(monitorSelfSlider, colW*4, rowH*3, colW*15, rowH);
+	elRect(monitorSelfToggle, colW*0.5, rowH*3, colW*3.5, rowH);
+	elRect(monitorSelfSlider, colW*4.5, rowH*3, colW*15, rowH);
 
-	elRect(sessionInput, colW, rowH*6, colW*2.5, rowH);
-	elRect(joinSessionBtn, colW*4, rowH*6, colW*2.5, rowH);
-	elRect(createSessionBtn, colW*7, rowH*6, colW*2.5, rowH);
-	elRect(leaveSessionBtn, colW*10, rowH*6, colW*2.5, rowH);
+	elRect(sessionInput, colW, rowH*5.5, colW*3, rowH*2);
+	elRect(joinSessionBtn, colW*4.5, rowH*5.5, colW*3, rowH*2);
+	elRect(createSessionBtn, colW*8, rowH*5.5, colW*3, rowH*2);
+	elRect(leaveSessionBtn, colW*11.5, rowH*5.5, colW*3, rowH*2);
+	elRect(recordBtn, colW*15, rowH*5.5, colW*3, rowH*2);
 
-	// qualitySlider.position(colW, rowH*8);
-  	// qualitySlider.style('width', int(colW*18).toString()+'px');
-  	// qualitySlider.style('height', int(rowH).toString()+'px');
+	sessionInput.style('font',  pxFont(colW, rowH*1.5) + ' "Courier New"');
+	joinSessionBtn.style('font',  pxFont(colW, rowH) + ' "Courier New"');
+	createSessionBtn.style('font',  pxFont(colW, rowH) + ' "Courier New"');
+	leaveSessionBtn.style('font',  pxFont(colW, rowH) + ' "Courier New"');
+	recordBtn.style('font',  pxFont(colW, rowH) + ' "Courier New"');
 
 }
 
@@ -215,6 +269,27 @@ function leaveSession() {
 		"event": "leave-session"
 	}
 	Bela.control.send(leaveRequest);
+}
+
+function record() {
+	var request
+	if (isRecording) {
+		request = {
+			"event": "stop-recording"
+		}
+		recordBtn.style('color', color(0));
+		recordBtn.html("Record")
+		isRecording = false
+	}
+	else {
+		request = {
+			"event": "start-recording"
+		}
+		recordBtn.style('color', color("red"));
+		recordBtn.html("Recording...")
+		isRecording = true
+	}
+	Bela.control.send(request);
 }
 
 function pxStr(val) {
